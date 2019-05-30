e681628f5fe2fa9d2447a617e4aca181557f07ca
Kadosaki Yoshiki <myutos44@gmail.com>
2019-05-30 07:47:40 +0000

    Finish basic information

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
   root    'static_pages#home'
-  get     '/signup', to: 'users#new'
-  get     '/login', to: 'sessions#new'
-  post    '/login', to: 'sessions#create'
-  delete  '/logout', to: 'sessions#destroy'
+  get     '/signup',  to: 'users#new'
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
+  get '/edit-basic-info/:id', to: 'users#edit_basic_info', as: :basic_info
+  patch 'update-basic-info', to: 'users#update_basic_info'
+  
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
